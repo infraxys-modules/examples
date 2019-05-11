168734de2044cd273139f29058b4222adacff0e8
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-05-11 11:17:40 +0200

    Initial

---
+++ auto-source/string_value.sh
@@ -0,0 +1,10 @@
+
+function show_string_value() {
+    local function_name="show_this_message" title message;
+    import_args "$@"; # populate local variables and name them as the argument name
+                      # (--title 'title' creates a local variable called 'title')
+    check_required_arguments "$function_name" title message; # exit with error if one of these 2 variables is empty
+
+    show_message_dialog --title "$instance_label.toUpperCase()" \
+        --message "Attribute '$instance_label' has value '$value'.";
+}
